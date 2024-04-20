06342fc896face4ea4572df44e1e3c4e76995fef
S.R.Eshwar Prasad <141043413+SREshwarPrasad@users.noreply.github.com>
2024-04-20 20:27:54 +0530

    BFORD.cpp

--- BFORD.cpp
+++ BFORD.cpp
@@ -1,87 +1,88 @@
-# include <iostream>
-#include <bits/stdc++.h>
-# define inf 1000
-# define max 100
-using namespace std;
+#include<iostream>
+#define max 100
+#define inf 10000
+using  namespace std; 
+
+int i,j; 			//iterating purpose
+int u,v; 			//from and to edge
+int nv, ne; 			//no of vertices and edges
+int f,t, weight; 		//from, to and weight of each edge
+int d[max], pi[max];		//d, pi table with relaxation
+int w[max][max]; 
+int s;				//source vertex
+
 
 struct edge
 {
-	int u,v,wt;
+	int u;
+	int v;
+	int wt;
 }e[max];
 
 
-int d[max]; 
-int w[max][max]; 
-int s;
-int nv,ne; //no of vertices and edges
-int pi[max];
-
 int main()
 {
-	int f,t,weight; //edge and weight 
-	int i,j; //loop
-	int u,v; 
-	
-	cout<<"Bellman Ford Algorithm"<<endl;
-
-	cout<<"Number of Vertices and Edges "<<endl;
-	cin>>nv>>ne;
-	cout<<"Source Vertex "<<endl;
-	cin>>s;
-	for (i=1;i<=nv;i++)
+	cout<<"BELLMAN FORD: "<<endl<<endl;
+	cout<<"No of vertices: "; cin>>nv;
+	cout<<"No of edges: "; cin>>ne;
+	cout<<"Source vertex? "; cin>>s;
+	for(i=1; i<=nv; i++)
 	{
 		d[i]=inf;
 		pi[i]=-1;
 	}
 	d[s]=0;
 	
-	for (i=1;i<=ne;i++)
+	for(i=1; i<=ne;i++)
 	{
-
-		cout<<"Edge "<<i<<" ? "<<endl;
-		cin>>f>>t;
-		cout<<"Weight "<<endl;
-		cin>>weight;
+		cout<<"Edge "<<i<<endl;
+		cout<<"From: "; cin>>f;
+		cout<<"To: "; cin>>t;
+		cout<<"Weight: "; cin>>weight;
+		cout<<endl;
 		
 		w[f][t]=weight;
 		e[i].u=f;
 		e[i].v=t;
-		e[i].wt=weight;
+		e[i].wt= weight;
 	}
-	for (i=1;i<=nv;i++)
+	for(i=1; i<=nv;i++)
 	{
-		for (j=1;j<=ne;j++)
+		for(j=1; j<=ne; j++)
 		{
-			u=e[j].u;
+			u=e[j].u; 
 			v=e[j].v;
-			if (d[v]>d[u]+w[u][v])
+			if(d[v]> d[u] + w[u][v])
 			{
 				d[v]=d[u]+w[u][v];
-				pi[v]=u;
+				pi[i]=u;
 			}
 		}
 	}
-	
 	int cycle=0;
-	for (j=1;j<=ne;j++)
+	for(j=1; j<=ne; j++)
 	{
 		u=e[j].u;
 		v=e[j].v;
-		if (d[v]>d[u]+w[u][v])
+		if(d[v] > d[u]+w[u][v])
 		{
 			cycle=1;
 			break;
-		}	
+		}
 	}
 	
-	if (!cycle)
+	if(!cycle)
 	{
-		cout<<"Shortest distance from the vertex "<<s<<" to every other vertices "<<endl;
-		for (i=2;i<=nv;i++)
-			cout<<i<<"\t"<<d[i]<<endl;
+		for(i=2; i<=nv; i++)
+		{
+			cout<<" "<<i<<"\t"<<d[i]<<endl;
+		}
 	}
 	else
-		cout<<"Graph contains negative cycle "<<endl;
+	{
+		cout<<"Negative";
+	}
+	
 	
 	return 0;
 }
